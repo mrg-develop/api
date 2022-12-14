311d5ff86dcee3a95d86226fc091ed76a513cb5b
Ubuntu <ubuntu@ip-172-31-0-221.ec2.internal>
2022-12-14 15:23:25 +0000

    with token validation

---
+++ app/Http/Controllers/Api/LoginController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function login(Request $request)
+    {
+        $this->validateLogin($request);
+
+        if (Auth::attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'token' => $request->user()->createToken($request->name)->plainTextToken, // creación del toeken en función de $request->name, que es el dispositivo desde donde se conecta
+                'message' => 'Success'
+            ]);
+
+            return reponse->json([
+                'message' => 'Unathorized'
+            ], 401);
+        }
+
+
+    }
+
+    public function validateLogin(Request $request)
+    {
+        return $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+            'name' => 'required'
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Api\V1\PostController as PostV1;
+use App\Http\Controllers\Api\V2\PostController as PostV2;
+use App\Http\Controllers\Api\LoginController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -18,4 +20,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('v1/posts', App\Http\Controllers\Api\V1\PostController::class)->only(['index', 'show', 'destroy']);
+Route::apiResource('v1/posts', PostV1::class)->only(['index', 'show', 'destroy'])
+    ->middleware('auth:sanctum');
+
+Route::apiResource('v2/posts', PostV2::class)->only(['index', 'show'])
+    ->middleware('auth:sanctum');
+
+Route::post('login', [App\Http\Controllers\Api\LoginController::class, 'login']);
